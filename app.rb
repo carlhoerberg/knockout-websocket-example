39531dbf694b0c030cf6024c997429ad76f8efb5
Carl Hörberg <carl.hoerberg@gmail.com>
2010-10-15 10:29:30 +0200

    Delete and Edit support

---
+++ app.rb
@@ -0,0 +1,58 @@
+require 'rubygems'
+require 'sinatra'
+require 'beanstalk-client'
+require 'dm-core'
+require 'dm-migrations'
+require 'dm-serializer'
+require 'json'
+
+class Contact 
+  include DataMapper::Resource
+	property :id, Serial
+	property :name, String
+	property :phone, String
+end
+
+configure do 
+	DataMapper.finalize
+	DataMapper.setup(:default, ENV['DATABASE_URL'] || "sqlite3://#{Dir.pwd}/dev.db")
+	DataMapper.auto_upgrade!
+end
+
+get '/' do
+	send_file(File.join('public', 'index.html'))
+end
+
+get '/contacts' do
+	cache_control :no_store
+	content_type :json
+	Contact.all.to_json
+end
+
+post '/contact/create' do
+	c = Contact.create params
+	broadcast("add", params[:id], c)
+end
+
+post '/contact/delete/:id' do
+	puts params
+	c = Contact.get(params[:id])
+	c.destroy
+	broadcast("delete", params[:id])
+end
+
+post '/contact/edit/:id' do
+	c = Contact.get(params[:id])
+	c.update params
+	c.save
+	broadcast("update", params[:id], c)
+end
+
+helpers do 
+	def broadcast(action, id, data = nil) 
+		bt = Beanstalk::Pool.new(['localhost:11300'])
+		json = {:action => action, :id => id, :item => data}.to_json
+		bt.put json 
+		"OK"
+	end
+end
--- socket.rb
+++ socket.rb
@@ -4,6 +4,13 @@ require 'em-jack'
 
 EventMachine.run do
 	@channel = EM::Channel.new
+
+	jack = EMJack::Connection.new
+	jack.each_job do |msg|
+		@channel.push msg.body
+		jack.delete msg
+	end
+
 	EventMachine::WebSocket.start(:host => "127.0.0.1", :port => 8080) do |ws|
 		ws.onopen do
 			sid = @channel.subscribe { |msg| ws.send msg }
